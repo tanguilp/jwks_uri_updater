4672f2f32f87390102cbe0296fbb2d3a9cd4c6c3
Niklas Johansson <raphexion@gmail.com>
2023-07-05 22:57:53 +0200

    Update and relax dependencies  (#2)
    * Change use Mix.Config to import Config
    
    * Relax ex_doc and dialyxir requirements
    
    * Remove dependency on poison
    
    * Update all dependencies

--- mix.exs
+++ mix.exs
@@ -29,11 +29,10 @@ defmodule JWKSURIUpdater.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:hackney, "~> 1.0", only: :dev},
       {:jose_utils, "~> 0.1"},
-      {:poison, "~> 4.0"},
       {:tesla, "~> 1.0"}
     ]
   end
